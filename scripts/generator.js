c935a53b182079be9a75a0c7a1732404705f572b
slobber <me@nandi.wang>
2025-06-15 19:20:58 +0800

    update

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,74 +1,44 @@
-const iconNames = require("../src/iconNames.js");
+import { cwd } from "process";
+import { iconNames } from "../src/iconNames.js";
+import fs from "fs";
 
-const fs = require("fs");
+const themes = {
+  outline: JSON.parse(
+    fs.readFileSync(
+      `${cwd()}/node_modules/@tabler/icons/tabler-nodes-outline.json`
+    )
+  ),
+  filled: JSON.parse(
+    fs.readFileSync(
+      `${cwd()}/node_modules/@tabler/icons/tabler-nodes-filled.json`
+    )
+  ),
+};
 
-const license = process.env.FALICENSE || "free";
-const libraries = [];
-if (license === "pro") {
-  libraries.push(require("@fortawesome/pro-solid-svg-icons"));
-  libraries.push(require("@fortawesome/pro-duotone-svg-icons"));
-  libraries.push(require("@fortawesome/pro-light-svg-icons"));
-  libraries.push(require("@fortawesome/pro-thin-svg-icons"));
-  libraries.push(require("@fortawesome/pro-regular-svg-icons"));
-  iconNames.HolderOutlined = { name: "holder", fa: "faGripDotsVertical" };
-  iconNames.EnterOutlined = { name: "enter", fa: "faTurnDownLeft" };
-} else {
-  libraries.push(require("@fortawesome/free-solid-svg-icons"));
-}
-fs.rmSync("./dist", { recursive: true, force: true });
-const template = `
-export default {
-  name: '{{name}}',
-  theme: 'outlined',
-  icon: {
-    tag: 'svg',
-    attrs: { viewBox: '0 0 {{width}} {{height}}', focusable: 'false' },
-    children: [
-      {
-        tag: 'path',
-        attrs: {
-          {{rotate}}
-          d: '{{path}}'
-        }
-      }
-    ]
+fs.rmSync(`${cwd()}/dist`, { recursive: true, force: true });
+Object.keys(iconNames).forEach((key) => {
+  const tiStyle = iconNames[key].theme === "filled" ? "filled" : "outline";
+  const tiName = iconNames[key].ti;
+  const data = {
+    icon: {
+      tag: "svg",
+      attrs: { viewBox: "0 0 24 24", focusable: "false" },
+      children: themes[tiStyle][tiName].map((x) => ({
+        tag: x[0],
+        attrs: x[1],
+      })),
+    },
+    name: iconNames[key].name,
+    theme: iconNames[key].theme || "outlined",
+  };
+  const fileContent = `export default ${JSON.stringify(data)}`;
+  const folder = `${cwd()}/dist/icons`;
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder, { recursive: true });
   }
-}
-`;
-libraries.forEach((set) => {
-  Object.keys(iconNames).forEach((key) => {
-    const icon = set[iconNames[key].fa];
-    if (!icon) {
-      console.log(
-        `\x1b[31m${iconNames[key].fa}\x1b[0m doesn't exist in \x1b[32m${set.prefix}\x1b[0m set, the ant design will use the default icon.`
-      );
-      return;
-    }
-    const data = {
-      name: iconNames[key].name,
-      width: icon.icon[0],
-      height: icon.icon[1],
-      theme: iconNames[key].theme || "outlined",
-      path: icon.icon[4],
-      rotate: iconNames[key].rotate
-        ? `transform:"rotate(${iconNames[key].rotate} ${icon.icon[0] / 2} ${
-            icon.icon[1] / 2
-          })",`
-        : "",
-    };
-    const fileContent = template.replace(/\{\{(\w*)\}\}/g, function (m, key) {
-      return data.hasOwnProperty(key) ? data[key] : "";
-    });
-    const folder = `dist/${set.prefix}-icons`;
-    if (!fs.existsSync(folder)) {
-      fs.mkdirSync(folder, { recursive: true });
-    }
-    fs.writeFileSync(`${folder}/${key}.js`, fileContent);
-  });
-  console.log(
-    `\x1b[32mIcons '${set.prefix}' are generated\x1b[0m.`
-  );
+  fs.writeFileSync(`${folder}/${key}.js`, fileContent);
 });
+console.log(`\x1b[32mIcons are generated\x1b[0m.`);
 
 fs.createReadStream("src/index.js").pipe(fs.createWriteStream("dist/index.js"));
 fs.createReadStream("src/iconNames.js").pipe(
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const path = require("path");
 const iconNames = require("./iconNames");
-const alias = (name = 'fas') => {
+const alias = () => {
   const aliasIcons = {};
   Object.keys(iconNames).forEach((icon) => {
     aliasIcons[`@ant-design/icons-svg/es/asn/${icon}`] = path.resolve(
       __dirname,
-      `${name}-icons/${icon}`
+      `icons/${icon}`
     );
     aliasIcons[`@ant-design/icons-svg/lib/asn/${icon}`] = path.resolve(
       __dirname,
-      `${name}-icons/${icon}`
+      `icons/${icon}`
     );
   });
   return aliasIcons;
